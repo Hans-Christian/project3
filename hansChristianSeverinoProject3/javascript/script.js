8391bcb6390388a5991cba915849218df3df0e05
HansChristian <hc.sev15@gmail.com>
2020-11-10 21:09:49 -0500

    Initial commit

---
+++ hansChristianSeverinoProject3/javascript/script.js
@@ -0,0 +1,24 @@
+// Use $(function(){}).
+$(function(){
+
+})
+
+// Attach an event listener to the form that will execute a function once the input[type="submit"] is clicked.
+    // Prevent the default browser refresh with e.preventDefault().
+    // Call randomizer exercise function, then assign the return value to a variable.
+    // Display the return value to the specific div.
+
+// The user will have to choose a body part they wish to exercise. For example, arms, legs, back, etc.
+
+// Initialize each option will be nested in an array inside of an object.
+    // const exercises = [bodypart: `biceps`, "exercise name": `bicep curls`, sets: 4, reps: 10, instructions:...]
+
+// Use if/else statements. Will handle invalid inputs.
+
+// Based on the user's choice, create a function that will take in an argument from the user’s input which will generate a random exercise, along with reps, and sets.
+
+// Filter exercises based on user selection.
+
+// Generate a random number using the Math.floor(), based on the exercise array.
+
+// Return exercise.
